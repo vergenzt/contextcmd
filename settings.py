62e6ce244f7c6773db8e563eb8ef1ece1d0880ac
Tim Vergenz <vergenzt@gmail.com>
2013-10-11 08:36:20 -0400

    Start describing system

--- settings.py
+++ settings.py
@@ -1,5 +1,14 @@
 from contextcmd import ArgRules, Rule
 
+'''
+A rule consists of a path on which the rule applies, and the default arguments
+to add for that path (and an optional `id`). `extend` determines whether to
+apply the rule to subdirectories as well, and `exclude` determines rules to
+exclude on the application of the current rule.
+
+
+'''
+
 ls = ArgRules(rules=[
     Rule('/', args='--color=auto --group-directories-first', extend=True),
     Rule('/home/tim', id='dereference', args='--dereference', extend=True),
